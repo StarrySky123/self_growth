258adac3ed0b27f716e0eea5840f004873501354
xuanxuan <18851738876@163.com>
2024-03-06 16:52:00 +0800

    --新建springboot测试工程

--- springboot-test-demo/src/main/java/com/xuanyuan/springboot/test/demo/controller/TestController.java
+++ springboot-test-demo/src/main/java/com/xuanyuan/springboot/test/demo/controller/TestController.java
@@ -1,5 +1,15 @@
 package com.xuanyuan.springboot.test.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
+
+    @RequestMapping("/test")
+    public String test (){
+        return "这是一个好的开始";
+    }
+
 }
