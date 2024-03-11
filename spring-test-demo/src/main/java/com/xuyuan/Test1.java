d7db3e25cce8ea6fc60dea4f65816d28a069ac9e
xuanxuan <18851738876@163.com>
2024-03-11 10:50:29 +0800

    --新建springboot测试工程

--- spring-test-demo/src/main/java/com/xuyuan/Test1.java
+++ spring-test-demo/src/main/java/com/xuyuan/Test1.java
@@ -3,8 +3,9 @@ package com.xuyuan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.xuyuan.service.UserService;
-
+import javax.annotation.PostConstruct;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Test1 {
 
@@ -17,7 +18,17 @@ public class Test1 {
 
         for (Field field : userService.getClass().getFields()) {
             if(field.isAnnotationPresent(Autowired.class)){
+                //field.set(userService,??);
+            }
+        }
 
+        for (Method method : userService.getClass().getMethods()) {
+            if(method.isAnnotationPresent(PostConstruct.class)){
+                try {
+                    method.invoke(userService,null);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
