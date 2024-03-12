cd305d6cc0ccdbfe3fd03c28c30a1dab1a36b7db
xuanxuan <18851738876@163.com>
2024-03-12 22:03:38 +0800

    aspect

--- spring-test-demo/src/main/java/com/xuyuan/service/OrderService.java
+++ spring-test-demo/src/main/java/com/xuyuan/service/OrderService.java
@@ -2,6 +2,7 @@ package com.xuyuan.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class OrderService {
@@ -9,6 +10,7 @@ public class OrderService {
     @Autowired
     private SendService sendService;
 
+    @Transactional
     public void test(){
         sendService.test();
         //System.out.println("test");
