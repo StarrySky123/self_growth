a1f19c209254fbd965af1a5071d54d6bae51af8c
xuanxuan <18851738876@163.com>
2024-03-12 15:08:37 +0800

    aspect

--- spring-test-demo/src/main/java/com/xuyuan/AppConfig.java
+++ spring-test-demo/src/main/java/com/xuyuan/AppConfig.java
@@ -4,9 +4,17 @@ import com.xuyuan.service.OrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @ComponentScan("com.xuyuan")
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class AppConfig {
 
     @Bean
@@ -18,4 +26,26 @@ public class AppConfig {
     public OrderService orderService2(){
         return new OrderService();
     }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate();
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://192.168.162.111:3306/xuyuan?characterEncoding=utf-8&amp;useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("ai710109");
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource());
+        return  transactionManager;
+    }
+
 }
