42c73ea5b5d199fbd6dd82449243de9eaf6003f5
xuanxuan <18851738876@163.com>
2024-03-13 22:27:21 +0800

    write spring

--- write-spring/src/main/java/com/xuanyuan/test/Test.java
+++ write-spring/src/main/java/com/xuanyuan/test/Test.java
@@ -7,7 +7,7 @@ import com.xuanyuan.test.service.UserService;
 public class Test {
 
     public static void main(String[] args) {
-        //扫描->创建单例bean
+        //扫描->创建单例bean BeanDefinition BeanPostProcessor
         XuanYuanApplicationContext applicationContext = new XuanYuanApplicationContext(AppConfig.class);
 
         UserInterface userService = (UserInterface) applicationContext.getBean("userService");
