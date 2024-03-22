cd89e3660ea13a076322c66bf44b83e3c2e00b59
xuanxuan <18851738876@163.com>
2024-03-22 13:30:31 +0800

    核心概念解析

---
+++ spring-test-demo/src/main/java/com/xuyuan/spring/Test.java
@@ -0,0 +1,12 @@
+package com.xuyuan.spring;
+
+import com.xuyuan.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //扫描 创建非懒加载单例bean
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+}
